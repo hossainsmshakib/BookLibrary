64fbe7a8d799eb459fedfbe1a7d93aa28296e22e
S. M. Shakib Hossain <smsakib2403@gmail.com>
2023-02-09 13:25:56 +0600

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,24 @@
 fun createBorrowerHistory(books: List<Books>, borrowers: List<Borrower>, newList: List<Int>): MutableMap<Int, Int> {
-    val borrowerHistory = mutableListOf<BorrowerHistory>()
     val result = mutableMapOf<Int, Int>()
-    var currentBook = 0
-    for (userId in newList) {
-        val borrower = borrowers.firstOrNull { it.borrowerId == userId }
-        if (borrower != null) {
-            if (currentBook >= books.size) {
-                continue
-            }
-            borrowerHistory.add(BorrowerHistory(books[currentBook].bookId, borrower.borrowerId))
-            result[userId] = books[currentBook].bookId
-            currentBook++
+
+    for (i in 0 until books.size) {
+        val bookId = books[i].bookId
+        if (i >= newList.size) {
+            continue
         }
+        val userId = newList[i]
+        val borrower = borrowers.firstOrNull { borrower -> borrower.borrowerId == userId }
+        if (borrower == null) {
+            continue
+        }
+        result[userId] = bookId
     }
     return result
 }
 fun main() {
     // create dummy of books
     val books = mutableListOf<Books>()
-    for (i in 1..50) {
+    for (i in 1..5) {
         books.add(Books(i, "Book $i", "Author $i"))
     }
 
@@ -28,10 +28,9 @@ fun main() {
         borrowers.add(Borrower(i, "Borrower Name $i", i + 20))
     }
 
-    val newList = listOf(100, 56, 5, 1, 2, 4, 25, 3, 1000)
+    val newList = listOf(10, 12, 1, 2, 3)
     val borrowerHistory = createBorrowerHistory(books, borrowers, newList)
     for ((userId, bookId) in borrowerHistory) {
-        println("BookId: $bookId assigned to UserId: $userId ")
+        println("BookId: $bookId assigned to UserId: $userId")
     }
 }
-
